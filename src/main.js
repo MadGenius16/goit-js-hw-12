ddfa375e7cf9c93ce3ed955c2d5dabefc8337528
MadGenius16 <aleksandrco777@gmail.com>
2024-06-23 18:20:41 +0300

    1

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// import axios from 'axios';
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
@@ -7,15 +9,18 @@ import 'izitoast/dist/css/iziToast.min.css';
 import {getPhotos} from './js/pixabay-api';
 import {showGallery} from './js/render-functions';
 
-const form = document.querySelector('.form');
-export const gallery = document.querySelector('.gallery');
-
 export const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
 
-const loaderBox = document.querySelector('.loader');
+
+  const form= document.querySelector('.form');
+  const btnLoadMore= document.querySelector('.js-btn-load');
+  export const gallery= document.querySelector('.gallery');
+  const loaderBox= document.querySelector('.loader');
+  const searchBtn= document.querySelector('.search-btn');
+
 
 function showLoader() {
   loaderBox.classList.remove('visually-hidden');
@@ -24,24 +29,70 @@ function showLoader() {
 export function closeLoader() {
   loaderBox.classList.add('visually-hidden');
 }
-form.addEventListener('submit', event => {
+
+function showLoadBtn() {
+  btnLoadMore.classList.remove('.visually-hidden')
+}
+function closeLoadBtn() {
+  btnLoadMore.classList.add('.visually-hidden')
+}
+
+let currentPage=1;
+let query;
+const perPage=15
+let maxPage;
+
+form.addEventListener('submit', async event => {
   event.preventDefault();
   showLoader();
   gallery.innerHTML = '';
-  const query = event.currentTarget.elements.inputname.value.trim();
+  currentPage = 1;
+   query = event.target.elements.inputname.value.trim();
   if (query !== '') {
-    getPhotos(query)
-      .then(res => {
-        showGallery(res.hits);
-        form.reset();
-      })
-      .catch(error =>
-        iziToast.error({
+    try{
+      const data = await getPhotos(query, currentPage)
+      maxPage = Math.ceil(data.totalHits / perPage);
+      showGallery(data.hits);
+      form.reset();
+    }
+      catch { iziToast.error({
           message: 'Sorry, there are no images matching your search query. Please try again!',
           color: 'red',
           position: 'topRight',
         })
-      );
-    form.reset();
+      }
+      closeLoader();
+      showLoadBtn()
+      // e.target.reset();
+    }
   }
-});
+);
+btnLoadMore.addEventListener("click", async()=>{
+currentPage++;
+showLoader()
+showLoadBtn()
+try {
+  const data = await getPhotos(query, currentPage)
+  const markup = showGallery(data.hits);
+  gallery.insertAdjacentHTML('beforeend', markup)
+} catch {
+  console.log('error');
+}
+closeLoader()
+updateBtnStatus()
+})
+
+function updateBtnStatus() {
+  if (currentPage >= maxPage) {
+    closeLoadBtn();
+
+    if (maxPage) {
+      iziToast.info({
+        title: 'The End!',
+        message: 'End of collection!',
+      });
+    }
+  } else {
+    showLoadBtn();
+  }
+}
